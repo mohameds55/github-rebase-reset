e008a8b2201e217f1d6f19b16a3060126f822667
mohameds55 <mohameds407518@gmail.com>
2025-12-17 14:56:03 +0200

    Add ClearComponent with template, styles, and tests

---
+++ src/app/pages/clear/clear.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clear',
+  standalone: true,
+  imports: [],
+  templateUrl: './clear.component.html',
+  styleUrl: './clear.component.scss'
+})
+export class ClearComponent {
+
+}
